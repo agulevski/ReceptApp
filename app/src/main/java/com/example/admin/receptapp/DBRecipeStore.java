7b9b96b5d4534006fae41a270356605ed1852102
agulevski <gusguleal@student.gu.se>
2017-11-15 11:15:21 +0100

    More DB

--- app/src/main/java/com/example/admin/receptapp/DBRecipeStore.java
+++ app/src/main/java/com/example/admin/receptapp/DBRecipeStore.java
@@ -1,8 +1,20 @@
 package com.example.admin.receptapp;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by admin on 2017-11-13.
  */
 
-public class DBRecipeStore {
+public class DBRecipeStore implements  RecipeStore {
+
+    private SQLiteDatabase database;
+    private DBRecipeHelper dbHelper;
+    private String[] allColumns = { DBRecipeHelper.COLUMN_ID, DBRecipeHelper.COLUMN_MEMBER_NAME };
+
+    public DBRecipeStore(Context context){
+        dbHelper = new DBRecipeHelper(context);
+    }
+
 }
